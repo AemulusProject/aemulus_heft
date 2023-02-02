a5dfb96d026d3e24b3c7aa6c6a4ef938cc2e3b08
Joseph DeRose <joe.derose13@gmail.com>
2023-02-01 16:50:44 -0800

    working on emu api

---
+++ aemulus_heft/heft_emu.py
@@ -0,0 +1,267 @@
+import numpy as np
+import h5py as h5
+from scipy.interpolate import interp1d
+
+
+class LPTEmulator(object):
+
+    """Main emulator object"""
+
+    def __init__(
+        self,
+        forceLPT=True,
+        training_file=None,
+    ):
+        """
+        Initialize the emulator object. Default values for all kwargs were
+        used for fiducial results in 2101.11014, so don't change these unless
+        you have a good reason!
+
+        Kwargs:
+            forceLPT : bool
+                Whether to transition to pure LPT at low k.
+            training_file: str
+                File that the training data is stored in.
+        """
+        self.nspec = 14
+        self.forceLPT = forceLPT
+
+        with h5.File(training_file, "r") as fp:
+            self.coeff = fp["pce_coefficients"][:]
+            self.exp = fp["pce_exponents"][:]
+            self.param_mean = fp["param_mean"][:]
+            self.param_mult = fp["param_mult"][:]
+            self.pcs_mean = fp["pcs_mean"][:]
+            self.pcs_mult = fp["pcs_mult"][:]
+            self.evec_spec = fp["evec_spec"][:]
+            self.k = fp["k"][:]
+            self.kmin = fp["k_min"][:]
+            self.kmax = fp["k_max"][:]
+
+        self.evec_spline = interp1d(
+            self.k, self.evec_spec, axis=1, fill_value=0, bounds_error=False
+        )
+
+    def predict(self, k, cosmo, spec_lpt, k_lpt=None):
+        """
+        Make predictions from a trained emulator given a vector of wavenumbers and
+        a cosmology.
+
+        Args:
+            k : array-like
+                1d vector of wave-numbers. Maximum k cannot be larger than
+                self.kmax. For k < self.kmin, predictions will be made using
+                velocileptors, for self.kmin <= k < self.kmax predictions
+                use the emulator.
+            cosmo : array-like
+                Vector containing cosmology/scale factor in the order
+                (ombh2, omch2, w0, ns, 10^9 As, H0, logmnu, loga).
+            spec_lpt : array-like
+                LPT predictions for spectra from velocileptors at the specified cosmology
+                call.
+            k_lpt: array-like
+                k values LPT predictions are evaluated at if different than k.
+
+        Output:
+            Emulator predictions for the 10 basis spectra of the 2nd order lagrangian bias expansion.
+            Order of spectra is 1-1, delta-1, delta-delta, delta2-1, delta2-delta, delta2-delta2
+            s2-1, s2-delta, s2-delta2, s2-s2.
+        """
+
+        if len(cosmo.shape) == 1:
+            x = cosmo[:, np.newaxis]
+        else:
+            # to keep API same as before for aemulus alpha
+            x = cosmo.T
+
+        if np.any(np.max(k) > self.k):
+            if np.all(np.max(k) > self.k):
+                raise (
+                    ValueError(
+                        "Trying to compute spectra beyond the maximum value of the emulator!"
+                    )
+                )
+            else:
+                print(
+                    "{} is greater than k_max for at least one spectrum. Setting P(k>k_max)=0".format(
+                        np.max(k)
+                    )
+                )
+
+        # scale input variables
+        x_n = (x - self.param_mean[:, np.newaxis]) * self.param_mult[:, np.newaxis]
+        in_domain = (-1.0001 <= x_n) & (x_n <= 1.0001)
+
+        if not in_domain.all():
+            raise (ValueError("{} is not in training domain".format(x[~in_domain])))
+
+        # evaluate lpt spectra at correct k if not already
+        if (k_lpt is not None) & (np.sum(k != k_lpt) > 0):
+            lpt_interp = interp1d(k_lpt, spec_lpt, axis=-1, fill_value="extrapolate")
+            spectra_lpt = lpt_interp(k)
+        else:
+            spectra_lpt = spec_lpt
+
+        if spectra_lpt.shape[-1] != len(k):
+            raise (
+                ValueError(
+                    "Trying to feed in lpt spectra computed at different k than the desired outcome!"
+                )
+            )
+
+        # interpolate PCs
+        evecs = self.evec_spline(k)
+
+        lambda_surr_normed = np.sum(
+            self.coeff[..., np.newaxis]
+            * np.prod(x_n ** self.exp[..., np.newaxis], axis=-2)[:, np.newaxis, :],
+            axis=-2,
+        )
+        lambda_surr = (
+            lambda_surr_normed / self.pcs_mult[..., np.newaxis]
+            + self.pcs_mean[..., np.newaxis]
+        )
+        simoverlpt_emu = np.einsum("bkp, bpc->cbk", evecs, lambda_surr)
+
+        pk_emu = np.zeros_like(spectra_lpt)
+        pk_emu[:] = spectra_lpt
+
+        # set spectra above kmax to 0
+        pk_emu[:, k[np.newaxis, :] > self.kmax[:, np.newaxis]] = 0
+
+        # Enforce agreement with LPT
+        if self.forceLPT:
+            pk_emu[..., k[np.newaxis, :] > self.kmin[:, np.newaxis]] = (
+                10 ** (simoverlpt_emu) * pk_emu
+            )[..., k[np.newaxis, :] > self.kmin[:, np.newaxis]]
+        else:
+            pk_emu[...] = 10 ** (simoverlpt_emu) * pk_emu[...]
+
+        return pk_emu, lambda_surr
+
+    def basis_to_full(self, k, btheta, emu_spec, cross=True):
+        """
+        Take an LPTemulator.predict() array and combine with bias parameters to obtain predictions for P_hh and P_hm.
+
+
+        Inputs:
+        -k: set of wavenumbers used to generate emu_spec.
+        -btheta: vector of bias + shot noise. See notes below for structure of terms
+        -emu_spec: output of LPTemu.predict() at a cosmology / set of k values
+        -halomatter: whether we compute only P_hh or also P_hm
+
+        Outputs:
+        -pfull: P_hh (k) or a flattened [P_hh (k),P_hm (k)] for given spectrum + bias params.
+
+
+        Notes:
+        Bias parameters can either be
+
+        btheta = [b1, b2, bs2, SN]
+
+        or
+
+        btheta = [b1, b2, bs2, bnabla2, SN]
+
+        Where SN is a constant term, and the bnabla2 terms follow the approximation
+
+        <X, nabla^2 delta> ~ -k^2 <X, 1>.
+
+        Note the term <nabla^2, nabla^2> isn't included in the prediction since it's degenerate with even higher deriv
+        terms such as <nabla^4, 1> which in principle have different parameters.
+
+        """
+        if len(btheta) == 4:
+            b1, b2, bs, sn = btheta
+            # Cross-component-spectra are multiplied by 2, b_2 is 2x larger than in velocileptors
+            bterms_hh = [
+                0,
+                1,
+                0,
+                2 * b1,
+                b1**2,
+                0,
+                b2,
+                b2 * b1,
+                0.25 * b2**2,
+                0,
+                2 * bs,
+                2 * bs * b1,
+                bs * b2,
+                bs**2,
+            ]
+
+            # hm correlations only have one kind of <1,delta_i> correlation
+            bterms_hm = [1, 0, b1, 0, 0, b2 / 2, 0, 0, 0, bs, 0, 0, 0, 0]
+
+            pkvec = emu_spec
+
+        else:
+            b1, b2, bs, bk2, sn = btheta
+            # Cross-component-spectra are multiplied by 2, b_2 is 2x larger than in velocileptors
+            bterms_hh = [
+                0,
+                1,
+                0,
+                2 * b1,
+                b1**2,
+                0,
+                b2,
+                b2 * b1,
+                0.25 * b2**2,
+                0,
+                2 * bs,
+                2 * bs * b1,
+                bs * b2,
+                bs**2,
+                0,
+                2 * bk2,
+                2 * bk2 * b1,
+                bk2 * b2,
+                2 * bk2 * bs,
+            ]
+
+            # hm correlations only have one kind of <1,delta_i> correlation
+            bterms_hm = [
+                1,
+                0,
+                b1,
+                0,
+                0,
+                b2 / 2,
+                0,
+                0,
+                0,
+                bs,
+                0,
+                0,
+                0,
+                0,
+                bk2,
+                0,
+                0,
+                0,
+                0,
+            ]
+
+            pkvec = np.zeros(shape=(self.nspec + 4, len(k)))
+            pkvec[: self.nspec] = emu_spec
+
+            # IDs for the <nabla^2, X> ~ -k^2 <1, X> approximation.
+            if cross:
+                nabla_idx = [0, 2, 5, 9]
+            else:
+                nabla_idx = [1, 3, 6, 10]
+
+            # Higher derivative terms
+            pkvec[self.nspec :] = -(k**2) * pkvec[nabla_idx]
+
+        if cross:
+            bterms_hm = np.array(bterms_hm)
+            pfull = np.einsum("b, bk->k", bterms_hm, pkvec)
+
+        else:
+            bterms_hh = np.array(bterms_hh)
+            pfull = np.einsum("b, bk->k", bterms_hh, pkvec) + sn
+
+        return pfull
---
+++ aemulus_heft/utils.py
@@ -0,0 +1,111 @@
+from velocileptors.EPT.cleft_kexpanded_resummed_fftw import RKECLEFT
+from scipy.interpolate import interp1d
+from classy import Class
+import numpy as np
+
+def _cleft_pk(k, p_lin):
+    '''
+    Returns a spline object which computes the cleft component spectra.
+    Computed either in "full" CLEFT or in "k-expanded" CLEFT (kecleft)
+    which allows for faster redshift dependence.
+    Args:
+        k: array-like
+            Array of wavevectors to compute power spectra at (in h/Mpc).
+        p_lin: array-like
+            Linear power spectrum to produce velocileptors predictions for.
+            Note that we require p_lin at the redshift that you wish
+            to make predictions for, because in cosmologies with neutrinos
+            a constant linear growth rescaling no longer works.
+    Returns:
+        cleft_aem : InterpolatedUnivariateSpline
+            Spline that computes basis spectra as a function of k.
+        cleftobt: CLEFT object
+            CLEFT object used to compute basis spectra.
+    '''
+    cleftobj = RKECLEFT(k, p_lin)
+    cleftobj.make_ptable(D=1, kmin=k[0], kmax=k[-1], nk=1000)
+    cleftpk = cleftobj.pktable.T
+
+    cleftpk[2, :] /= 2
+    cleftpk[6, :] /= 0.25
+    cleftpk[7, :] /= 2
+    cleftpk[8, :] /= 2
+
+    cleftspline = interp1d(cleftpk[0], cleftpk, fill_value='extrapolate')
+
+    return cleftspline, cleftobj
+
+def lpt_spectra(k, z, cosmo, pkclass=None):
+    """_summary_
+
+    Args:
+        k (array-like): 1d vector of wave-numbers. Maximum k cannot be larger than
+                    self.kmax. For k < self.kmin, predictions will be made using
+                    velocileptors, for self.kmin <= k < self.kmax predictions
+                    use the emulator.
+        z (float): redshift
+        cosmo (array-like): Vector containing cosmology in the order
+                            (ombh2, omch2, w0, ns, 10^9 As, H0, logmnu).  
+        pkclass (Class object, optional): Class for this cosmology if already run.
+                                          Defaults to None.
+    """
+    h = cosmo[5]/100
+    cosmo_dict = {
+        'h': h,
+        'Omega_b': cosmo[0] / h**2,
+        'Omega_cdm': cosmo[1] / h**2,
+        'N_ur': 0.00641,
+        'N_ncdm': 1,
+        'output': 'mPk mTk',
+        'z_pk': '0.0,99',
+        'P_k_max_h/Mpc': 20.,
+        'm_ncdm': cosmo[7]/3,
+        'deg_ncdm': 3,
+        'T_cmb': 2.7255,
+        'A_s': cosmo[4] * 10**-9,
+        'n_s': cosmo[3],
+        'Omega_Lambda': 0.0,
+        'w0_fld': cosmo[2],
+        'wa_fld': 0.0,
+        'cs2_fld': 1.0,
+        'fluid_equation_of_state': "CLP"
+    }
+    
+    if pkclass == None:
+        pkclass = Class()
+        pkclass.set(cosmo_dict)
+        pkclass.compute()
+
+    kt = np.logspace(-3, 1, 100)
+
+    pk_m_lin = np.array(
+        [
+            pkclass.pk_lin(ki, np.array([z])) * h ** 3
+            for ki in kt * h
+        ]
+    )
+    pk_cb_lin_zb = np.array(
+        [
+            pkclass.pk_cb_lin(ki, np.array([z])) * h ** 3
+            for ki in kt * h
+        ]
+    )
+
+    cleft_m_spline, _ = _cleft_pk(kt, pk_m_lin)
+    cleft_cb_spline, _ = _cleft_pk(kt, pk_cb_lin_zb)
+    
+    pk_m_cleft = cleft_m_spline(k)[1:]
+    pk_cb_cleft = cleft_cb_spline(k)[1:]
+
+    s_m_map = {0: 0, 2: 1, 5: 3, 9: 6}
+    s_cb_map = {1: 0, 3: 1, 4: 2, 6: 3, 7: 4, 8: 5, 10: 6, 11: 7, 12: 8, 13: 9}
+        
+    pk_cleft = np.zeros((14,len(k)))
+    for s in np.arange(14):
+	    if s in [0, 2, 5, 9]:
+            pk_cleft[s, :] = pk_m_cleft[s_m_map[s]]
+        else:
+            pk_cleft[s, :] = pk_cb_cleft[s_cb_map[s]]
+
+    return pk_cleft
+
