b41c898f03d5d8159034bb74598720882e3cc570
Joseph DeRose <joe.derose13@gmail.com>
2023-02-13 13:58:09 -0800

    syntax

--- aemulus_heft/heft_emu.py
+++ aemulus_heft/heft_emu.py
@@ -137,7 +137,7 @@ class HEFTEmulator(object):
         pk_emu = np.zeros_like(spectra_lpt)
         pk_emu[:] = spectra_lpt
         # set spectra above kmax to 0
-        pk_emu[k[np.newaxis, :] > self.kmax[:, np.newaxis]] = 0
+        pk_emu[..., k[np.newaxis, :] > self.kmax[:, np.newaxis]] = 0
 
         # Enforce agreement with LPT
         if self.forceLPT:
